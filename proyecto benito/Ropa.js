1861b3b7fe88f3df16d519ef22a0f64389cb419e
brayan-uwu <114691714+brayan-uwu@users.noreply.github.com>
2023-03-09 22:08:43 -0600

    UWU

--- proyecto benito/Ropa.js
+++ proyecto benito/Ropa.js
@@ -2,10 +2,10 @@ class Ropas extends Sucursaal {
     viene;
     edad;
     Corte;
-    constructor(nam, u, tal, Marcaa, ) {
-        super(nam, u, tal, Marcaa);
+    constructor(nam, u, tal, Marc, ) {
+        super(nam, u, tal, Marc);
         this.viene = tal;
-        this.Marca = Marcaa;
+        this.edad = Marc;
 
     }
 
